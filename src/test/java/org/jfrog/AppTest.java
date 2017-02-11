99334228b723c8f5b5f0c977957445432333a063
Anton Sipachev <retrozet@gmail.com>
2017-02-11 23:49:21 +0300

    Initial

---
+++ src/test/java/org/jfrog/AppTest.java
@@ -0,0 +1,16 @@
+package org.jfrog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by 1 on 11.02.2017.
+ */
+class AppTest {
+    @Test
+    void main() {
+
+    }
+
+}
